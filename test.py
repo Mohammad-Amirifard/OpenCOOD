383d19a1d020aa6c2383bf9e36f59b2901017417
Mohammad-Amirifard <amirifard.mo@gmail.com>
2025-12-20 18:52:25 +0100

    removed additional files

--- test.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-checkpoint = torch.load(r"opencood\logs\point_pillar_intermediate_V2VAM_nocompressionV2_2025_11_14_21_14_17\net_epoch1.pth", map_location="cpu")
-print(type(checkpoint))
-print(checkpoint.keys())
-print("Epoch:", checkpoint.get("epoch"))
-print("Loss:", checkpoint.get("loss"))
